7a8c3610e409362212047810bd758d51d6673ce6
akanshasharma2211 <57264099+akanshasharma2211@users.noreply.github.com>
2022-05-02 15:40:17 +0530

    Update singleinstancevariables.tf

--- singleinstancevariables.tf
+++ singleinstancevariables.tf
@@ -11,21 +11,6 @@ variable "aws_secret_key" {
   description = "aws_secret_key"
   type        = string
 }
-variable "ec2_instance_type" {
-  type        = string
-  default     = "t2.micro"
-}
-variable "ec2_ami" {
-  description = "ec2_ami"
-  type        = string
-  default     = "ami-0a3277ffce9146b74"
-}
-
-variable "storage_size" {
-  description = "storage_size"
-  type        = string
-  default     = "8"
-}
 variable "aws_sns_topic" {
   description = "sns_topic"
   type        = string
