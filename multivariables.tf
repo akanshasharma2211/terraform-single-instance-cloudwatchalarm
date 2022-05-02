0279b9ef409d28593694b80c20f6a9e0aa90f2ec
akanshasharma2211 <57264099+akanshasharma2211@users.noreply.github.com>
2022-05-02 15:39:12 +0530

    Update multivariables.tf

--- multivariables.tf
+++ multivariables.tf
@@ -11,22 +11,6 @@ variable "aws_secret_key" {
   description = "aws_secret_key"
   type        = string
 }
-variable "ec2_instance_type" {
- description = "ec2_instance_type"
-  type        = string
-  default     = "t3a.small"
-}
-variable "ec2_ami" {
-  description = "ec2_ami"
-  type        = string
-  default     = "ami-04c84f136b3c9d872"
-}
-
-variable "storage_size" {
-  description = "storage_size"
-  type        = string
-  default     = "8"
-}
 variable "aws_sns_topic" {
   description = "sns_topic"
   type        = string
